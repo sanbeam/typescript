4f7d77756852de579679712c367e67020a257ce4
Sanjeev BA <iamsanjeev@gmail.com>
2021-09-22 12:54:51 +0900

    What we avoid.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,1 +1,12 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number){
+    // if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect input');
+    // }
+    return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+
+const result = add(num1, num2);
+console.log(result);
