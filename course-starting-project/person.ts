f935180f6ea318469deddde90c22793cda76d02a
Sanjeev BA <iamsanjeev@gmail.com>
2021-09-22 14:31:16 +0900

    Union types.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- course-starting-project/person.ts
+++ course-starting-project/person.ts
@@ -17,8 +17,23 @@ const person:  {
 let favActs: string[];
 favActs = person.hobbies;
 
+let anyArr: any[];
+anyArr = [5, person, 10, "Sanjeev"];
+
 console.log(person);
 
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase());
 }
+
+function combine(input1: number|string, input2: string|number){
+    if(typeof input1 === 'number' && typeof input2 === 'number' ){
+        return input1 + input2;
+    }else{
+        return input1.toString() + input2.toString();
+    }
+}
+
+console.log(combine(1, 2));
+console.log(combine(1, "Sanjeev"));
+console.log(combine( "Sanjeev", 324));
