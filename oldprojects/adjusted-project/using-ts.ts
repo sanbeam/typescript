ea2535ede163e835449993eebd9c66dccb57b79c
Sanjeev BA <iamsanjeev@gmail.com>
2021-09-23 07:45:30 +0900

    Using dist folder for out.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- oldprojects/adjusted-project/using-ts.ts
+++ oldprojects/adjusted-project/using-ts.ts
@@ -2,10 +2,10 @@ const button = document.querySelector("button")! as HTMLInputElement;
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
+function add3(num1: number, num2: number) {
   return num1 + num2;
 }
 
 button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
+  console.log(add3(+input1.value, +input2.value));
 });
