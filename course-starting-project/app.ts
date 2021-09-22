22136c835f6cc2c0c1375f69c2a48985929e579e
Sanjeev BA <iamsanjeev@gmail.com>
2021-09-22 13:04:55 +0900

    Print phrases.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,12 +1,19 @@
-function add(n1: number, n2: number){
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
     // if(typeof n1 !== 'number' || typeof n2 !== 'number'){
     //     throw new Error('Incorrect input');
     // }
-    return n1 + n2;
+
+    if(showResult){
+        console.log(phrase + n1 + n2);
+    }else{
+        return n1 + n2;
+    }
 }
 
 const num1 = 5;
 const num2 = 2.8;
+const printResult = true;
+const printPhrase = "Result is";
 
-const result = add(num1, num2);
-console.log(result);
+const result = add(num1, num2, printResult, printPhrase);
+console.log(printPhrase + result);
