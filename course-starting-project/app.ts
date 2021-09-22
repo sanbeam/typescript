081b17179cba98b9e8a0f15f9b9585530eeae77c
Sanjeev BA <iamsanjeev@gmail.com>
2021-09-22 16:47:07 +0900

    Unknown fix.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -46,10 +46,16 @@ addAndHandle(10, 20, (result: number) => {
 
 
 // let userInput: unknown; wont work
-let userInput: any;
+// let userInput: any;
+let userInput: unknown;
 let userName: string;
 
 userInput = 5;
 userInput = 'Max';
-userName = userInput;
+
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+
 
